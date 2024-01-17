51719db4bbfad0b0b0e9e614430ba2cae69e459a
Andrew Davis <andrew4d3@gmail.com>
2024-01-17 08:40:55 -0300

    Lesson 80: using objects as states in reducers

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,20 +3,27 @@
 
 import * as React from 'react'
 
-const countReducer = (count, step) => {
-  return count + step;
+const countReducer = (state, newState) => {
+  return {
+    ...state,
+    ...newState
+  };
 }
 
 function Counter({initialCount = 0, step = 1}) {
   // 🐨 replace React.useState with React.useReducer.
   // 💰 React.useReducer(countReducer, initialCount)
-  const [count, changeCount] = React.useReducer(countReducer, 0);
+  const [state, changeState] = React.useReducer(countReducer, {
+    count: initialCount,
+  });
+
+  const { count } = state;
 
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => changeCount(step)
+  const increment = () => changeState({ count: count + step })
   return <button onClick={increment}>{count}</button>
 }
 
